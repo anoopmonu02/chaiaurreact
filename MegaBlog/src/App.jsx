732cf8506d0cb39e2a017fcac4fdcc994b37a3b0
Anoop <anoopmonu02@gmail.com>
2024-02-18 19:14:23 +0530

    configure redux

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,16 +1,41 @@
 
+import React, { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import { Footer, Header } from './components'
+import {Outlet} from 'react-router-dom'
 
 function App() {
   //process.env.REACT_APP_APPWRITE_URL - if created by create react app
   //import.meta.env.VITE_SOME_KEY - if created by vite
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Blog Apps</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) =>{
+      if (userData) {
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+        <div className='w-full block'>
+            <Header />
+            <main>
+              <Outlet />
+            </main>
+            <Footer />
+        </div>
+    </div>
+  ) : null
 }
 
 export default App
