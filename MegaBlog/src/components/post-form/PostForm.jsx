20c04d470313ea18de328f1f72af365ce6396504
Anoop <anoopmonu02@gmail.com>
2024-02-22 10:28:42 +0530

    Post-form design completed

--- MegaBlog/src/components/post-form/PostForm.jsx
+++ MegaBlog/src/components/post-form/PostForm.jsx
@@ -19,8 +19,120 @@ function PostForm({post}) {
     const navigate = useNavigate()
     const userData = useSelector(state => state.user.userData)
 
+    const submit = async (data) => {
+      if(post) {
+        const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null
+        if(file) {
+          appwriteService.removeFile(post.featuredImage)
+        }
+        const dbPost = await appwriteService.updatePost(post.$id, {
+          ...data,
+          featuredImage: file? file.$id : undefined,
+          if(dbPost) {
+            navigate(`/post/${dbPost.$id}`)
+          }
+        })
+
+      } else{
+        const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null
+        if(file) {
+          const fileId = file.$id
+          data.featuredImage = fileId
+          const dbPost = await appwriteService.createPost({
+            ...data,
+            userId: userData.$id,
+          })
+          if(dbPost) {
+            navigate(`/post/${dbPost.$id}`)
+          }
+        }
+      }
+    }
+
+    const slugTransformer = useCallback(value => {
+      if(value && typeof value ==='string') {
+        //Removing the space or special char with - in slug field
+        return setValue("slug", value.trim().toLowerCase().replace(/\s+/g, '-'))
+      }
+      return ''
+    }, [])
+
+    React.useEffect(() => {
+      const subscription = watch((value, {name}) =>{
+        if(name ==='title') {
+          setValue('slug',slugTransformer(value.title, {shouldValidate: true}))
+        }
+      })
+
+      return () => {
+        subscription.unsubscribe()
+      }
+    },[watch, slugTransformer, setValue])
+
   return (
-    <div>PostForm</div>
+    <form onSubmit={handleSubmit(submit)} className='flex flex-wrap'>
+      <div className='w-2/3 px-2'>
+        <Input 
+          label="Title: "
+          placeholder="Enter title"
+          type="text"
+          className="mb-4"
+          {...register("title",{
+            required: true
+          })}
+        />
+
+        <Input 
+          label="Slug: "
+          placeholder="Enter slug"
+          type="text"
+          className="mb-4"
+          {...register("slug",{
+            required: true
+          })}
+          onInput = {(e) =>{
+            setValue('slug',slugTransformer(e.currentTarget.value, {shouldValidate: true}))
+          }}
+        />
+
+        <RTE 
+        label="Content: " name="content" control={control} defaultValue={getValues('content')}
+        />
+      </div>
+
+      <div className='w-1/3 px-2'>
+          <Input 
+          label="Featured Image: "
+          type="file"
+          className="mb-4"
+          accept="image/png, image/jpeg, image/jpg, image/gif"
+          {...register("image",{
+            required: !post
+          })}
+          />
+
+          {post && (
+            <div className='w-full mb-4'>
+              <img src={appwriteService.getFilePreview(post.featuredImage)} alt={post.title} className='rounded-lg' />
+            </div>
+          )}
+
+          <Select 
+            label="Status "
+            options={["active","inactive"]}
+            className="mb-4"
+            {...register("status",{
+              required: true
+            })}
+          />
+
+          <Button type="submit" className="w-full" bgColor={post?"bg-green-500":undefined}>
+              {post? "Update Post" : "Create Post"}
+          </Button>
+
+      </div>    
+
+    </form>
   )
 }
 
